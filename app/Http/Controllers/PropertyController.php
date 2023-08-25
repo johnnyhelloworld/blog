6f0c7f22dfd8a6cfe6d014c637bf7b6bc3ba0288
thomasR92 <thomas.riehl92@gmail.com>
2023-08-25 14:12:21 +0200

    Adding property details

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -2,16 +2,45 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Property;
+use Illuminate\Http\Request;
+use App\Http\Requests\SearchPropertiesRequest;
 
 class PropertyController extends Controller
 {   
-    public function index(){
+    public function index(SearchPropertiesRequest $request){
 
+        $query = Property::query()->orderBy('created_at','desc');
+        if ($price = $request->validated('price')){
+            $query = $query->where('price', '<=', $price);
+        }
+
+        if ($surface = $request->validated('surface')){
+            $query = $query->where('surface', '>=', $surface);
+        }
+
+        if ($rooms = $request->validated('rooms')){
+            $query = $query->where('rooms', '>=', $rooms);
+        }
+
+        if ($title = $request->validated('title')){
+            $query = $query->where('title', 'like', "%{$title}%");
+        }
+
+        return view('property.index',[
+            'properties' => $query->paginate(16),
+            'input' => $request->validated()
+        ]);
     }
 
     public function show(string $slug, Property $property){
-        
+        $expectedSlug = $property->getSLug();
+        if ($slug != $expectedSlug){
+            return to_route('property.show',['slug' => $expectingSlug, 'property' => $property]);
+        }
+
+        return view('property.show',[
+            'property' => $property
+        ]);
     }
 }
